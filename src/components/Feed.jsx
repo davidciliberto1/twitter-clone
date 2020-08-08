135001db31273763f6078aee78b486391aa80f6a
davidciliberto1 <davidciliberto@gmail.com>
2020-08-08 12:58:11 -0300

    layoutcss

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,19 +4,21 @@ import ImageSearchOutlinedIcon from "@material-ui/icons/ImageSearchOutlined";
 import GifOutlinedIcon from "@material-ui/icons/GifOutlined";
 import EqualizerOutlinedIcon from "@material-ui/icons/EqualizerOutlined";
 import SentimentVeryDissatisfiedOutlinedIcon from "@material-ui/icons/SentimentVeryDissatisfiedOutlined";
-import TodayOutlinedIcon from '@material-ui/icons/TodayOutlined';
+import TodayOutlinedIcon from "@material-ui/icons/TodayOutlined";
 import "../css/feed.css";
 
 function Feed() {
   return (
-    <div className="feed">
+    <div className="allfeed">
+        
       <div className="feed">
-        <div className="header">
+      <div className="header">
           <h1>Home</h1>
           <div className="iconStars">
             <StarsOutlinedIcon />
           </div>
         </div>
+        
         <div className="twitterBox">
           <div className="imagenPerfil"></div>
           <div className="inputTweet">
@@ -24,28 +26,68 @@ function Feed() {
               className="inputTweet"
               type="text"
               placeholder="Whats happening?"
+              
             />
           </div>
           <div className="opcionesTweet">
             <div className="iconImagen">
-              <button className='optionButton'><ImageSearchOutlinedIcon /></button>
+              <button className="optionButton">
+                <ImageSearchOutlinedIcon />
+              </button>
             </div>
-            <div className="iconGif" >
-              <button className='optionButton'><GifOutlinedIcon /></button>
+            <div className="iconGif">
+              <button className="optionButton">
+                <GifOutlinedIcon />
+              </button>
             </div>
             <div className="iconChart">
-              <button className='optionButton'><EqualizerOutlinedIcon /></button>
+              <button className="optionButton">
+                <EqualizerOutlinedIcon />
+              </button>
             </div>
             <div className="emoticon">
-              <button className='optionButton'><SentimentVeryDissatisfiedOutlinedIcon /></button>
+              <button className="optionButton">
+                <SentimentVeryDissatisfiedOutlinedIcon />
+              </button>
             </div>
             <div className="calendar">
-            <button className='optionButton'><TodayOutlinedIcon/></button>
+              <button className="optionButton">
+                <TodayOutlinedIcon />
+              </button>
             </div>
             <button className="buttonTweet">Tweet</button>
           </div>
         </div>
-        <div className="feedNews"></div>
+        
+      </div>
+      <div className="newsFeed">
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+          <h1>Feeeeeeed!!!</h1>
+
       </div>
     </div>
   );
