a07740b03f886e0124bd1b62d095d2d92eef9559
davidciliberto1 <davidciliberto@gmail.com>
2020-08-10 14:24:24 -0300

    reduce2

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -29,7 +29,7 @@ const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Metodo '.filter()' retorna un array nuevo con los objetos seleccionados true por la funcion()
 let numerosFiltrados = numeros.filter(x => x >= 7);
 
-const perros =JSON.stringify(mascotas.filter(x => x.edad < 5 ));
+
 
 // metodo '.map' retorna un array nuevo con todos los elementos del array antiguo atravezados por la funcion()
 // multiplicacion de todos los numeros del array usando .map
@@ -37,7 +37,7 @@ let numerosMapeados = numeros.map(x => x *2);
 let parejas = numeros.map(x => [x, x]);
 let edades = mascotas.map(x => x.edad);
 let filtroPerrosJovenes = edades.filter(x => x < 5);
-let perrosJovenes = mascotas.filter(x => x == filtroPerrosJovenes);
+let perrosJovenes = JSON.stringify(mascotas.filter(x => x.edad < 5));
 // metodo '.reducer()' retorna la sumatoria de todos los elementos de un array acumulando (acc) la suma dede uno mas otro partiendo desde el valor inicial acc + num o elemento, 0 o [] o {}
 let reduciendoNumeros = numeros.reduce((acc, num) => acc + num, 0)
 function Widgets() {
